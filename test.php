d74801fdee7a71e3701c63ade3261f2866399e9b
mukeshchaudhari <d3systems3@gmail.com>
2013-09-11 18:45:47 +0530

    Changes move to git

--- test.php
+++ test.php
@@ -1,12 +1,13 @@
-<?php 
-
-
-tests
-tests
-stts
-tstst
-st
-wstst
-
+<?php
+  use_helper('Text');
+  if($userDetail['is_company']) {
+    $path = sfConfig::get('app_companys_logo_folder_path', '/user_logo/');
+    $logo = $userDetail['company_logo'];
+    if(!$logo) {
+      $logo = "t_default.jpg";
+      $path = "/images/";
+    } else {
+      $logo = "t_".$logo;
+    }
+  }
 ?>
-
